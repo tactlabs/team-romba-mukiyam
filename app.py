798fb5449d63a3da61cc89b6db6e7029b027d360
Prakash-Ramanathan1211 <prashprakash1211@gmail.com>
2021-06-05 21:22:47 +0530

    zip file added

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for, abort, \
 from werkzeug.utils import secure_filename
 import extract_zip as ez
 import compare_util as cu
+import zipfile
 
 
 app = Flask(__name__)
@@ -30,12 +31,32 @@ def upload_files():
 @app.route('/uploads/<filename>')
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
-
+    
 @app.route('/extract')
 def imported_task():
     ez.extract_zip('uploads/client.zip')
     cu.startpy()
-    return 'check results folder'
+    create_zip()
+    return send_file('client_data_compared.zip', as_attachment=True)
+
+def get_all_file_paths(directory):
+    file_paths = []
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    return file_paths   
+
+def create_zip():
+    directory = 'results'
+    file_paths = get_all_file_paths(directory)
+    print('Following files will be zipped:')
+    for file_name in file_paths:
+        print(file_name)
+    with zipfile('client_data_compared.zip','w') as zip:
+        for file in file_paths:
+            zip.write(file)
+    print('All files zipped successfully!')
 
 if __name__ == '__main__':
     app.run(debug=True)
